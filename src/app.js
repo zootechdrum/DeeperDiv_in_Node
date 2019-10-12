6c090dba6f7edacc6777c6e1f920e2c591765cf2
Cesar Gomez <zootechdrum@gmail.com>
2019-10-12 15:13:40 -0700

    added 4 routes in node.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,14 @@ app.get('/help', (req,res) => {
     res.send("Hello Is on the way");
 })
 
+app.get('/about', (req,res) => {
+    res.send("about Is on the way");
+})
+
+app.get('/weather', (req,res) => {
+    res.send("wether Is on the way");
+})
+
 
 app.listen(3000, () => {
     console.log("Server is up and running")
