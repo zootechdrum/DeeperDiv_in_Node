5bff7dee1bb35110b8505b51759d504c80bb7b8e
Cesar Gomez <zootechdrum@gmail.com>
2019-10-12 15:23:59 -0700

    added json object to weather path

--- src/app.js
+++ src/app.js
@@ -2,23 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('', (req,res) => {
+app.get('', (req, res) => {
     res.send("Hello express");
 })
 
-app.get('/help', (req,res) => {
+app.get('/help', (req, res) => {
     res.send("Hello Is on the way");
 })
 
-app.get('/about', (req,res) => {
-    res.send("about Is on the way");
+app.get('/about', (req, res) => {
+    res.send(
+        "<h1>This is the about me sectionnos</h1>"
+    );
 })
 
-app.get('/weather', (req,res) => {
-    res.send("wether Is on the way");
-})
+app.get('/weather', (req, res) => {
+    res.send(
+
+
+        {
+            name: "cesar",
+            lastName: "Gomez",
+            add: function (a, b) {
+                return a + b;
+            }
+        }
+    )
+});
 
 
 app.listen(3000, () => {
-    console.log("Server is up and running")
-});
+        console.log("Server is up and running")
+    });
