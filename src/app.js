4b9ce02ebfc886b0540e8eba0d96ceb3335bfc55
Cesar Gomez <zootechdrum@gmail.com>
2019-10-27 11:19:18 -0700

    added fetch call on client side

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.get('/weather', (req, res) => {
         })
     }
     
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if (error){
             res.send({error})
         }
