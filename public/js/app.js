404f2c08fcfdbab61f4a45faf5f314bac63ee3a1
Cesar Gomez <zootechdrum@gmail.com>
2019-10-27 11:29:50 -0700

    added form to index.hbs

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-console.log("Client side Javascript is being loaded")
+console.log("Client Side Javascript")
+
+fetch('http://localhost:3000/weather?address=!').then((response, reject) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+document.querySelector('form')
