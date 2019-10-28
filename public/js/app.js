aba23f2fcc023bedba95cd7aaee74225b170614c
Cesar Gomez <zootechdrum@gmail.com>
2019-10-27 21:15:50 -0700

    added functionality to where weather only gets called on form submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,23 @@
 console.log("Client Side Javascript")
+let callWeather = (city)=> {
+    fetch(`http://localhost:3000/weather?address=${city}`).then((response, reject) => {
+        response.json().then((data) => {
+            console.log(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    })
+}
 
-fetch('http://localhost:3000/weather?address=!').then((response, reject) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+const weatherForm = document.querySelector('form');
+
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const city = search.value;
+
+    callWeather(city)
 })
-
-document.querySelector('form')
