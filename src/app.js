f9ad8dbab3565c84cdebefd6dc14eee578352616
Cesar Gomez <zootechdrum@gmail.com>
2019-11-28 13:59:17 -0800

    added style to img and header

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/help', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'Cesar Gomez',
+        title: 'AboutMe',
         name: 'Cesar Gomez'
     })
 })
